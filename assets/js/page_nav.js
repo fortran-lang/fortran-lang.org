8646cacb072941d63a2265f5ab76093d94c87b78
Laurence Kedward <laurence.kedward@bristol.ac.uk>
2020-05-31 09:20:22 +0000

    Jekyll build, commit ba198d035fe67bde3db1cd54c47406439231f4cb (on:push)

---
+++ assets/js/page_nav.js
@@ -0,0 +1,35 @@
+// If the current page contains an element with id="pagenav-sidebar"
+//  then this script will populate it with <li> elements
+//  containing links to all the <h2> elements on the current page
+if (!!document.getElementById("pagenav-sidebar")){
+
+    var headings = document.querySelectorAll("h2[id]");
+
+    for (var i = 0; i < headings.length; i++) {
+        document.getElementById("page-nav").innerHTML +=
+            '<li id="nav-' + headings[i].id + 
+                '"><a href="#' + headings[i].id + '">' +
+                headings[i].innerText +
+            '</a></li>';
+    }
+
+    $(document).ready(function() {
+    $(window).scroll(function() {
+
+        var found = false;
+        var scrollPos = $(window).scrollTop();
+        for (var i = 0; i < headings.length; i++) {
+
+        if (scrollPos >= headings[i].offsetTop){
+            found = true;
+            $("#nav-"+headings[i].id).addClass('current');
+        } else {
+            $("#nav-"+headings[i].id).removeClass('current');
+        }
+
+        }
+
+    });
+    });
+
+}
