19a4b89cbfc0f501531429cc1695287fa0375ed6
Milan Curcic <caomaco@gmail.com>
2021-10-25 08:34:07 +0000

    Jekyll build, preview pr/345 (on:pull_request)

---
+++ pr/345/assets/js/fortranlang.js
@@ -0,0 +1,38 @@
+fortranLang = {
+
+baseurl: '/pr/345',
+
+findGetParameter:
+    (function findGetParameter(parameterName) {
+        // Return a GET HTTP parameter
+        var result = null,
+            tmp = [];
+        location.search
+            .substr(1)
+            .split("&")
+            .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+            });
+        return result;
+    }),
+
+
+loadJSON:
+    (function loadJSON(url, onLoad) {
+        // Load JSON data from url
+    //
+        var json = null;
+        $.ajax({
+            'global': false,
+            'url': url,
+            'dataType': "json",
+            'success': onLoad,
+            'error': function(jzXHR,status) {
+                console.log('Failed to load json file at '+url);
+                console.log(' Text status: '+status);
+            }
+        });
+    })
+
+};
