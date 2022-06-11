37e813194085d430d76e56766787ddfda86f68ce
Henil Panchal <henilp105@gmail.com>
2022-06-11 22:41:17 +0530

    Learn section page

--- source/_static/index.js
+++ source/_static/index.js
@@ -20,7 +20,7 @@ document.getElementById('templates').innerHTML = `<table jput="t_template">
   <ul>
     <tr>
       <td><li>
-    <a href="{{url}" target="_blank"><b>
+    <a href="{{url}}" target="_blank"><b>
       {{name}}</b></a>
    {{description}}</td></li>
   </tr>
