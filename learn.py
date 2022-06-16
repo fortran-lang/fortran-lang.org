0e12db898f9ddcb5239d32d64bdbf32a34a00f21
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-06-16 21:31:39 +0530

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,5 +1,5 @@
 import yaml
 from pathlib import Path
 print("learn section")
-conf = yaml.safe_load(Path('./learning.yml').read_text())
+conf = yaml.safe_load(Path('_data/learning.yml').read_text())
 print(conf)
