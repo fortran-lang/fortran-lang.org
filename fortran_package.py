b7dbcda9ca4b190612334a498d4f2787c6b38104
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-07-12 12:41:52 +0530

    Update fortran_package.py

--- fortran_package.py
+++ fortran_package.py
@@ -4,7 +4,11 @@ from collections import Counter
 import json
 import requests
 from requests.structures import CaseInsensitiveDict
-import datetime
+from datetime import datetime
+import pytz
+from collections import OrderedDict
+import numpy as np
+
 #print("learn section")
 info = requests.get('https://raw.githubusercontent.com/fortran-lang/fortran-lang.org/master/_data/package_index.yml').text
 fortran_index = yaml.safe_load(info)
@@ -143,36 +147,36 @@ with open("fortran_package.json", "w") as f:
     json.dump(fortran_tags, f)
 with open("fortran_learn.json", "w") as f:
     json.dump(conf, f)
-def Sort_Tuple(tup):
-    lst = len(tup)
-    for i in range(0, lst):
 
-        for j in range(0, lst-i-1):
-            if (tup[j][0] > tup[j + 1][0]):
-                temp = tup[j]
-                tup[j]= tup[j + 1]
-                tup[j + 1]= temp
-    return tup
+fortran_monthly =[]
+fortran_commits =[]
+fpm_monthly =[]
+fpm_commits =[]
+stdlib_monthly =[]
+stdlib_commits =[]
 
-def plot_graphs(graph):
-  a=[]
-  login=[]
-  contributions=[]
-  info = requests.get('https://api.github.com/repos/fortran-lang/'+graph+'/contributors', headers=headers).text
+months = ["Unknown", "January","Febuary","March", "April","May","June","July","August","September","October","November","December"]
+def monthly_graph(repo):
+  info = requests.get('https://api.github.com/repos/fortran-lang/'+repo+'/stats/code_frequency').text
   d = json.loads(info)
-  for i in range(len(d)):
-    try:
-        a.append((d[i]['login'],d[i]['contributions']))
-    except:
-        print("")
-  Sort_Tuple(a)
-  for i in a:
-    login.append(i[0])
-    contributions.append(i[1])
+  c = 0
+  monthly_commits=[]
+  commits=[]
+  for i in range(0,len(d),4):
+    for j in range(4):
+      try:
+        c = c + d[i+j][1]
+      except:
+       print("")
+    date_time = datetime.fromtimestamp(d[i][0])
+    monthly = str(months[date_time.month])+" "+str(date_time.year)
+    commits.append(c)
+    monthly_commits.append(monthly)
+    c=0
   test_chart = {"data": [
       {
-        "x": login,
-        "y": contributions,
+        "x": monthly_commits,
+        "y": commits,
       }
     ],
     "layout": {
@@ -184,9 +188,20 @@ def plot_graphs(graph):
       }
     }
   }
+  if repo =='fortran-lang.org':
+    fortran_monthly = monthly_commits
+    fortran_commits = commits
+  if repo =='fpm':
+    fpm_monthly = monthly_commits
+    fpm_commits = commits
+   # print(fpm_monthly , fpm_commits)
+  if repo =='stdlib':
+    stdlib_monthly = monthly_commits
+    stdlib_commits = commits
+    #print(stdlib_monthly , stdlib_commits)
   print(test_chart)
   with open("source/charts/"+graph+".json", "w") as f:
-    json.dump(test_chart, f)
+        json.dump(test_chart, f)
 graphs =["fortran-lang.org","fpm","stdlib"]
 for i in graphs:
-  plot_graphs(i)
+  monthly_graph(i)
