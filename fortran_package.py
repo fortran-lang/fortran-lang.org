98d3967fb21b9fe02d18eae6154175511cd7a98f
Henil Panchal <37398093+henilp105@users.noreply.github.com>
2022-07-17 09:40:16 +0530

    Update fortran_package.py

--- fortran_package.py
+++ fortran_package.py
@@ -10,7 +10,6 @@ import pytz
 from collections import OrderedDict
 import numpy as np
 
-print(str(sys.argv))
 months = ["Unknown", "January","Febuary","March", "April","May","June","July","August","September","October","November","December"]
 #print("learn section")
 info = requests.get('https://raw.githubusercontent.com/fortran-lang/fortran-lang.org/master/_data/package_index.yml').text
@@ -156,45 +155,6 @@ fpm_monthly =[]
 fpm_commits =[]
 stdlib_monthly =[]
 stdlib_commits =[]
-
-
-def monthly_graph(repo):
-  info = requests.get('https://api.github.com/repos/fortran-lang/'+repo+'/stats/code_frequency', headers=headers).text
-  d = json.loads(info)
-  c = 0
-  monthly_commits=[]
-  commits=[]
-  for i in range(0,len(d),4):
-    for j in range(4):
-      try:
-        c = c + d[i+j][1]
-      except:
-       print("")
-    date_time = datetime.fromtimestamp(d[i][0])
-    monthly = str(months[date_time.month])+" "+str(date_time.year)
-    commits.append(c)
-    monthly_commits.append(monthly)
-    c=0
-  test_chart = {"data": [
-      {
-        "x": monthly_commits,
-        "y": commits,
-      }
-    ],
-    "layout": {
-      "margin": {
-        "t": 15,
-        "b": 30,
-        "r": 15,
-        "l": 35
-      }
-    }
-  }
-  with open("source/charts/"+repo+".json", "w") as f:
-        json.dump(test_chart, f)
-graphs =["fortran-lang.org","fpm","stdlib"]
-for i in graphs:
-  monthly_graph(i)
 
 contributor =[]
 contributor_repo={
