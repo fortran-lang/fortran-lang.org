81e13b7dc545f568d33152b346df4f7c54b03cca
urbanjost <urbanjost@comcast.net>
2022-08-19 05:08:01 +0000

    Jekyll build, preview pr/418 (on:pull_request)

---
+++ pr/418/assets/js/fortranlang.js
@@ -0,0 +1,22 @@
+fortranLang = {
+
+baseurl: '/pr/418',
+
+loadJSON:
+    (function loadJSON(url, onLoad) {
+        // Load JSON data from url
+    //
+        var json = null;
+        $.ajax({
+            'global': false,
+            'url': url,
+            'dataType': "json",
+            'success': onLoad,
+            'error': function(jzXHR,status) {
+                console.log('Failed to load json file at '+url);
+                console.log(' Text status: '+status);
+            }
+        });
+    })
+
+};
